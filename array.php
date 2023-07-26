e3ac0e6b7ffddcb7c9193bc462d6e31252800945
Ghiraldy <140327200+Ghiraldy@users.noreply.github.com>
2023-07-26 12:46:38 +0900

    Add files via upload

---
+++ array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h3>belajar looping (pengulangan)</h3>
+    <?php 
+ 
+    //penamaan isi array
+    $buah['semangka'] = "isinya merah";
+    $buah['jeruk'] = "rasanya manis";
+    $buah['apel'] = "warnanya merah";
+    $buah['anggur'] = "harganya mahal";
+ 
+    // menampilkan isi array yang bernama jeruk
+    echo $buah['jeruk'];
+ 
+    ?>
+</body>
+</html>
